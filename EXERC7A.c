7e21cd4fac0414a62b0cd4c4cb29c4a3014ec191
Eduardo Prado <eduprado15.ep@gmail.com>
2025-10-28 16:32:53 -0400

    questão 7a
    Inclusão questão 7a

--- EXERC7A.c
+++ EXERC7A.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int numero, i = 1;
 
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    printf("\nTabuada do %d:\n", numero);
+
+inicio: // rótulo usado para o goto
+    printf("%d x %d = %d\n", numero, i, numero * i);
+    i++;
+
+    if (i <= 10)
+        goto inicio;  // desvio condicional para repetir o bloco
+
+    return 0;
 }
